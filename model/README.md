918206e43572cc9ec93524fabf747dc85723d728
lingo <yll357019074@163.com>
2017-04-21 14:21:54 +0800

    change the interface: not seg

--- model/README.md
+++ model/README.md
@@ -1,19 +1,42 @@
 路径配置文件 cnn.conf：
 
-    segment_path
+    segment_path=xxx
 
-    parameter_path
+    parameter_path=xxx
 
-    embedding_path
+    embedding_path=xxx
 
-用法：
+用法1：
 
     from cnn import predict
 
     label = predict(text)
 
-格式：
+格式1：
 
-    text：用户说的所有语句，忽略客服的话（无需分词，utf-8编码或gbk编码，最好用utf-8）
+    text：多行对话语句，每行格式: talker\001\002\003content
+    
+    格式说明：talker为0或1，0表示客服，1表示用户；content需分词，词之间用空格分开，换行符为\n；编码为utf-8
+
+    例：
+
+    0\001\002\003您好
+
+    1\001\002\003您好 ， 我 要 投诉 你
+    
+    0\001\002\003好 的
 
     label：int标签，1、投诉；2、办理；3、查询；4、其他
+
+用法2：
+    from cnn import text_predict
+
+    label,result = text_predict(text)
+
+格式2：
+    
+    text: 纯文本内容(无需分词，utf-8编码)
+
+    label: int标签，1、投诉；2、办理；3、查询；4、其他
+
+    result: 每个类别的分数
--- model/cnn.py
+++ model/cnn.py
@@ -86,6 +86,18 @@ def trans2utf(text):
         sys.exit(2)
 
 
+def trans2words(text):
+    need = []
+    text = text.strip().split('\n')
+    for line in text:
+        line = line.strip().split('\001\002\003')
+        if len(line)==2 and line[0]=='1':
+            words = line[1].strip().split(' ')
+            for word in words:
+                need.append(word.decode('utf-8'))
+    return need
+
+
 def ConvLayer(q1,q2):
     output = T.dot(T.concatenate([q1, q2]), W_c) + b_c
     return output
@@ -105,7 +117,7 @@ def find_nd(score):
 输入：text
 输出：label
 '''
-def predict(text):
+def text_predict(text):
     # 文本转向量
     text = trans2utf(text)
     words = segmentor.segment(text)
@@ -138,6 +150,54 @@ def predict(text):
     result = model_predict(numpy.array(cur_vec))[0]
     # rule 2: 最高分过低，默认为3类
     if max(result)<0.41:
+        return 3,result
+    # rule 3: 识别为3类，但3类分数与次高分数相差很小，选用次高的类别
+    if result.argmax()==2:
+        pos,nd = find_nd(result)
+        if max(result)-nd<0.01:
+            return pos,result
+    return result.argmax()+1,result
+
+
+
+'''
+输入：segmented text
+输出：label
+'''
+def predict(text):
+    # 文本转向量
+    text = trans2utf(text)
+    words = trans2words(text)
+    cur_vec = []
+    cnt = 0
+    for word in words:
+        if word in word_embedding:
+            cur_vec.append(word_embedding[word])
+            cnt += 1
+        else:
+            temp = rng.uniform(low=-0.1,high=0.1,size=[50])
+            cur_vec.append(temp)
+    # print 'vector length:',len(cur_vec)
+    # rule 1: 词数过少，判定为4
+    if cnt < 2:
+        return 4
+
+    # 预测模型
+    px = T.matrix('px')
+    # convolution
+    conv_px_output, _ = theano.scan(fn=ConvLayer, sequences=[px[:-1],px[1:]])
+    # tanh and pooling
+    px_hidden = T.tanh(T.max(conv_px_output,axis=0))
+    # hidden layer
+    px_output = T.tanh(T.dot(px_hidden,W_h)+b_h)
+    # output layer 
+    px_softmax_output = T.nnet.softmax(px_output)
+    # function
+    model_predict = theano.function(inputs=[px],outputs=px_softmax_output)
+    
+    result = model_predict(numpy.array(cur_vec))[0]
+    # rule 2: 最高分过低，默认为3类
+    if max(result)<0.41:
         return 3
     # rule 3: 识别为3类，但3类分数与次高分数相差很小，选用次高的类别
     if result.argmax()==2:
@@ -146,7 +206,11 @@ def predict(text):
             return pos
     return result.argmax()+1
 
-
 if __name__ == '__main__':
-   print predict('移动怎么乱扣费啊'.decode('utf-8').encode('gb2312')) 
+   #print predict('1\001\002\003移动 怎么 乱 扣费 啊')
+   while(True):
+       order = raw_input('请输入文本:')
+       if order == 'exit':
+           break
+       print text_predict(order)
     
